f3885d883f92bae19d78358e7442e2cca59525c2
Dylan Smith <foliomedia@gmail.com>
2014-01-23 17:10:30 -0800

    Nearly working. Now need to figure out permissions to enable save to Desktop.

--- ScreenGroper/RZIAppController.h
+++ ScreenGroper/RZIAppController.h
@@ -8,12 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
+@class MASShortcutView;
+
 @interface RZIAppController : NSObject
 
+@property (nonatomic, strong) NSNumber *width;
+@property (nonatomic, copy) NSString *url;
 @property (nonatomic, strong) IBOutlet NSMenu *statusMenu;
 @property (nonatomic, strong) NSStatusItem *statusItem;
 @property (nonatomic, strong) NSImage *statusImage;
 @property (nonatomic, strong) NSImage *statusImageHighlight;
+@property (nonatomic, weak) IBOutlet MASShortcutView *shortcutView;
 
 -(IBAction)captureSafariURL:(id)sender;
 @end
--- ScreenGroper/RZIAppController.m
+++ ScreenGroper/RZIAppController.m
@@ -7,8 +7,26 @@
 //
 
 #import "RZIAppController.h"
+#import "MASShortcutView.h"
+#import "MASShortcutView+UserDefaults.h"
+#import "MASShortcut+UserDefaults.h"
+#import "MASShortcut+Monitoring.h"
+#include <Python.h>
+
+NSString *const RZIPreferenceKeyShortcut = @"ScreenGroperShortcut";
+NSDictionary static *RZIWidths;
+typedef enum {
+    MOBILE = 320,
+    TABLET = 768,
+    DESKTOP = 1280
+} screenWidths;
 
 @implementation RZIAppController
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    self.shortcutView.associatedUserDefaultsKey = RZIPreferenceKeyShortcut;
+}
 
 - (void)awakeFromNib
 {
@@ -25,7 +43,71 @@
 
 - (IBAction)captureSafariURL:(id)sender
 {
-    NSLog(@"sender:%@", [sender parent]);
+    NSString *widthKey = [[NSString alloc] initWithString:[sender title]];
+    _width = [RZIWidths objectForKey:widthKey];
+    
+    _url = [[NSString alloc] initWithString:[self safariURL]];
+    if ([_url length] == 0) {
+        [[NSAlert alertWithMessageText:NSLocalizedString(@"Nothing to Grope.\nPlease open a URL in Safari.", @"Alert message for constant shortcut")
+                         defaultButton:NSLocalizedString(@"OK", @"Default button for the alert on constant shortcut")
+                       alternateButton:nil otherButton:nil informativeTextWithFormat:@""] runModal];
+    } else {
+      [self runScript:@"webkit2png"];
+    }
+}
+
+- (NSString *)safariURL
+{
+    NSDictionary *dict;
+    NSAppleEventDescriptor *result;
+    
+    NSAppleScript *script = [[NSAppleScript alloc] initWithSource:
+                             @"\ntell application \"Safari\"\n\tget URL of document 1\nend tell\n"];
+    
+    result = [script executeAndReturnError:&dict];
+    
+    if ((result != nil) && ([result descriptorType] != kAENullEvent)) {
+        return [result stringValue];
+    }
+    
+    return @"";
+}
+
+-(void) runScript:(NSString*)scriptName
+{
+    NSTask *task;
+    task = [[NSTask alloc] init];
+    task.launchPath = @"/usr/bin/python";
+    
+//    NSArray *arguments;
+    NSString* newpath = [NSString stringWithFormat:@"%@/%@",[[NSBundle mainBundle] privateFrameworksPath], scriptName];
+    
+    NSString *desktop = [NSSearchPathForDirectoriesInDomains(NSDesktopDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+//    task.arguments = @[newpath, [NSString stringWithFormat:@"-W %@ -z 2.0 --delay=1 -F -D %@ %@", _width, desktop, _url]];
+//    NSArray *arguments = @[newpath, [NSString stringWithFormat:@"-W %@ -z 2.0 --delay=1 -F -D %@ %@", _width, desktop, _url]];
+    NSArray *arguments = @[newpath, [NSString stringWithFormat:@"-W %@", _width], @"-z 2.0", @"-F", [NSString stringWithFormat:@"-D %@", desktop], [NSString stringWithFormat:@"%@", _url]];
+//    NSArray *arguments = @[[NSString stringWithFormat:@"%@ -W %@ -z 2.0 --delay=1 -F -D %@ %@", newpath, _width, desktop, _url]];
+    task.arguments = arguments;
+    NSLog(@"args = %@", arguments);
+    task.arguments = arguments;
+//    task.arguments = @[newpath, [NSString stringWithFormat:@"%@", _url]];
+    
+    NSPipe *pipe;
+    pipe = [NSPipe pipe];
+    [task setStandardOutput: pipe];
+    [task setStandardInput:[NSPipe pipe]];
+    
+    NSFileHandle *file;
+    file = [pipe fileHandleForReading];
+    
+    [task launch];
+    
+    NSData *data;
+    data = [file readDataToEndOfFile];
+    
+    NSString *string;
+    string = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
+    NSLog (@"script returned:\n%@", string);
 }
 
 - (id)init
@@ -33,13 +115,30 @@
     self = [super init];
     if (self) {
         [self registerHotKey];
+        RZIWidths = @{
+                      @"Mobile": [NSNumber numberWithInt:MOBILE],
+                      @"Tablet": [NSNumber numberWithInt:TABLET],
+                      @"Desktop": [NSNumber numberWithInt:DESKTOP]
+                    };
     }
     return self;
 }
 
 - (void)registerHotKey
 {
-    NSLog(@"registerring...");
+    MASShortcut *shortcut = [MASShortcut shortcutWithKeyCode:kVK_ANSI_5 modifierFlags:NSCommandKeyMask | NSControlKeyMask | NSShiftKeyMask];
+    
+    self.shortcutView.associatedUserDefaultsKey = RZIPreferenceKeyShortcut;
+//    [MASShortcut registerGlobalShortcutWithUserDefaultsKey:RZIPreferenceKeyShortcut handler:^{
+        // Let me know if you find a better or more convenient API.
+//    }];
+    [MASShortcut addGlobalHotkeyMonitorWithShortcut:shortcut handler:^{
+        [_statusItem popUpStatusItemMenu:_statusMenu];
+//        NSString *msg = [[NSString alloc] initWithFormat:@"%@ has been pressed.", shortcut];
+//        [[NSAlert alertWithMessageText:NSLocalizedString(msg, @"Alert message for constant shortcut")
+//                         defaultButton:NSLocalizedString(@"OK", @"Default button for the alert on constant shortcut")
+//                       alternateButton:nil otherButton:nil informativeTextWithFormat:@""] runModal];
+    }];
 }
 
 @end
